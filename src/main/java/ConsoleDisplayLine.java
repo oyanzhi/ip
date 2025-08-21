2936f0cb65137faaaee042304cd3ca19be0c134f
oyanzhi <ongyanzhi15@gmail.com>
2025-08-21 21:36:30 +0800

    Add bot echoing

--- src/main/java/ConsoleDisplayLine.java
+++ src/main/java/ConsoleDisplayLine.java
@@ -1,14 +1,24 @@
+import java.lang.Math;
+
 public final class ConsoleDisplayLine {
     static final int LENGTH = 50;
     static final String CHAR = "-";
 
-    public static void printHorizontalLine() {
+    public static void printHorizontalLine(int spacing) {
+        int minimumLineLength = Math.max(spacing, LENGTH);
+
         System.out.println();
 
-        for (int i = 0; i < LENGTH; ++i) {
+        for (int i = 0; i < spacing; i++) {
+            System.out.print(" ");
+        }
+
+        for (int j = 0; j < minimumLineLength; j++) {
             System.out.print(CHAR);
         }
 
         System.out.println();
     }
+
+
 }
--- src/main/java/TrackerBot.java
+++ src/main/java/TrackerBot.java
@@ -1,14 +1,45 @@
+import java.util.Scanner;
+
 public class TrackerBot {
 
     public static void main(String[] args) {
         String name = "TrackerBot";
 
-        ConsoleDisplayLine.printHorizontalLine();
+        //Initial Greeting
+        ConsoleDisplayLine.printHorizontalLine(0);
         System.out.println("Hello! I'm "
                 + name
-                + "\nWhatcan I do for you?");
-        ConsoleDisplayLine.printHorizontalLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        ConsoleDisplayLine.printHorizontalLine();
+                + "\n"
+                + "What can I do for you?");
+        ConsoleDisplayLine.printHorizontalLine(0);
+
+        //Echo
+        String toExit = "bye";
+
+        Scanner inputScanner = new Scanner(System.in);
+
+        while (inputScanner.hasNextLine()) {
+            String userInput = inputScanner.nextLine();
+            int inputLength = userInput.length();
+
+            ConsoleDisplayLine.printHorizontalLine(inputLength);
+
+            //Bot Echos
+            for (int i = 0; i < inputLength; i++) {
+                System.out.print(" ");
+            }
+
+            //Exit Check
+            if (userInput.equals(toExit)) { //Exiting
+                System.out.println("Bye. Hope to see you again soon!");
+                ConsoleDisplayLine.printHorizontalLine(inputLength);
+                break;
+            } else { //Not Exiting
+                System.out.println(userInput);
+                ConsoleDisplayLine.printHorizontalLine(inputLength);
+            }
+
+        }
+
     }
 }
