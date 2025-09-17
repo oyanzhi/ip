05f56bfb82d12d2a6146ea29f561e3ffbecee110
oyanzhi <ongyanzhi15@gmail.com>
2025-09-17 13:39:12 +0800

    Add title to GUI

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("TrackerBot");
             fxmlLoader.<MainWindow>getController().setTrackerBot(trackerbot);
             stage.show();
         } catch (IOException e) {
